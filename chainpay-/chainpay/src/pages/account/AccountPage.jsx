8825dfd45998d9e18454b15d91be8dcb9431b440
pizzy11 <53659959+pizzy11@users.noreply.github.com>
2026-05-09 00:26:31 +0100

    Update AccountPage.jsx

--- chainpay-/chainpay/src/pages/account/AccountPage.jsx
+++ chainpay-/chainpay/src/pages/account/AccountPage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import toast from 'react-hot-toast'
 import { useAuth, PLANS } from '../context/AuthContext'
-import { getReferralStats, getEarningsSummary } from '../../lib/referrals'
+import { getReferralStats, getEarningsSummary } from '../lib/referrals'
 import { Logo, Spinner } from '../components/UI'
 
 export default function AccountPage() {
