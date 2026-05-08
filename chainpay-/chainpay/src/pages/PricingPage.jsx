a3c3127071e98c5f4b341c201cddd07d1b43502b
pizzy11 <53659959+pizzy11@users.noreply.github.com>
2026-05-09 00:18:52 +0100

    Update PricingPage.jsx

--- chainpay-/chainpay/src/pages/PricingPage.jsx
+++ chainpay-/chainpay/src/pages/PricingPage.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import toast from 'react-hot-toast'
 import { useAuth, PLANS } from '../../context/AuthContext'
-import { Logo } from '../../components/UI'
+import { Logo } from '../components/UI'
 
 const PLAN_ORDER = ['free', 'pro', 'business']
 
